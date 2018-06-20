10b6139fe1bee993b78199765c9a895d98221116
spointeau-neopost <38245678+spointeau-neopost@users.noreply.github.com>
2018-06-20 15:39:59 +0200

    Update slack

--- watson-slack.js
+++ watson-slack.js
@@ -16,13 +16,13 @@
 
 var Botkit = require('botkit');
 require('dotenv').load();
-var sharedCode = require('./handleWatsonResponse.js')();
+var sharedCode = require('./watson.js')();
 
 var middleware = require('botkit-middleware-watson')({
-    username: process.env.CONVERSATION_USERNAME,
-    password: process.env.CONVERSATION_PASSWORD,
+    username: process.env.ASSISTANT_USERNAME,
+    password: process.env.ASSISTANT_PASSWORD,
     workspace_id: process.env.WORKSPACE_ID,
-    version_date: '2016-09-20'
+    version_date: '2017-05-26'
 });
 
 var controller = Botkit.slackbot({
